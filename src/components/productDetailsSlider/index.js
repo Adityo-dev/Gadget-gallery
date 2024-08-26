0192ff12855649cb7302c5561c281ac99847e2c0
Aditto dev <computerscience01770@gmail.com>
2024-08-27 09:56:50 +1200

    productDetails responsive  [Done]

--- src/components/productDetailsSlider/index.js
+++ src/components/productDetailsSlider/index.js
@@ -33,31 +33,6 @@ function ProductDetailsSlider() {
     slidesToScroll: 1,
     initialSlide: 0,
     infinite: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
   };
 
   return (
