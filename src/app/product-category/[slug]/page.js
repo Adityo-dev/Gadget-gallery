a502e9ebdd790361edb7749bf88abd4d3ea3fde7
Aditto dev <computerscience01770@gmail.com>
2024-08-28 23:27:48 +1200

    product-category [Done]

--- src/app/product-category/[slug]/page.js
+++ src/app/product-category/[slug]/page.js
@@ -1,3 +1,14 @@
+const fetchProductCategory = async () => {
+  return [{ id: "1" }, { id: "2" }];
+};
+
+export async function generateStaticParams() {
+  const products = await fetchProductCategory();
+  return products.map((product) => ({
+    productCategory: product.id,
+  }));
+}
+
 const DynamicProductCategory = ({ params }) => {
   return (
     <div>
