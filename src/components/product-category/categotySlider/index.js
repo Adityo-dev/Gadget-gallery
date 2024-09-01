7b203fb7861e8b98323e84d3c603b9870d74b0a1
Aditto dev <computerscience01770@gmail.com>
2024-09-01 14:32:30 +1200

    product-category slider responsive [Done]

--- src/components/product-category/categotySlider/index.js
+++ src/components/product-category/categotySlider/index.js
@@ -37,24 +37,26 @@ const ProductCategorySlider = () => {
     slidesToScroll: 1,
     responsive: [
       {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 5,
+        },
+      },
+
+      {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
+          slidesToShow: 4,
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 800,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
+          slidesToShow: 3,
         },
       },
       {
-        breakpoint: 480,
+        breakpoint: 570,
         settings: {
           slidesToShow: 2,
         },
