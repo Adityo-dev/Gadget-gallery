7fb5873b4af7bcb7fff3d5c4a80576117792bb98
Aditto dev <computerscience01770@gmail.com>
2024-09-07 10:22:49 +1200

    add about  [Done]

---
+++ src/app/about/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+
+// import image
+
+const About = () => {
+  return (
+    <main>
+      <h2>About</h2>
+    </main>
+  );
+};
+
+export default About;
