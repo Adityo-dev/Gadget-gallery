5bccb1f17bd5d8a53e1262b7f5b7201298d77905
Aditto dev <computerscience01770@gmail.com>
2024-09-07 18:54:12 +1200

    add google map file   [Done]

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,56 +1,63 @@
+import GoogleMap from "@/components/googleMap";
 import styles from "./contact.module.css";
 
 const Contact = () => {
   return (
-    <section className={styles.allDataContainer}>
-      <div className={styles.infoContainer}>
-        <h2 className={styles.title}>Do you have some questions?</h2>
-        <p className={styles.subTitle}>
-          We are at your disposal 7 days a week!
-        </p>
+    <>
+      <section className={styles.allDataContainer}>
+        <div className={styles.infoContainer}>
+          <h2 className={styles.title}>Do you have some questions?</h2>
+          <p className={styles.subTitle}>
+            We are at your disposal 7 days a week!
+          </p>
 
-        <p className={styles.location}>
-          <span>29 SE 2nd Ave,</span>
-          <span> Miami, Florida 33131,</span>
-          <span>dhaka</span>
-        </p>
+          <p className={styles.location}>
+            <span>29 SE 2nd Ave,</span>
+            <span> Miami, Florida 33131,</span>
+            <span>dhaka</span>
+          </p>
 
-        <div className={styles.contactInfo}>
-          <p className={styles.gmailId}>gadgetgallery03@gmail.com</p>
-          <p className={styles.phoneNum}>+8801770365981</p>
+          <div className={styles.contactInfo}>
+            <p className={styles.gmailId}>gadgetgallery03@gmail.com</p>
+            <p className={styles.phoneNum}>+8801770365981</p>
+          </div>
         </div>
-      </div>
 
-      <div className={styles.inputFieldContainer}>
-        <div className={styles.nameAadEmailContainer}>
-          <input
-            className={styles.inputField}
-            type="text"
-            placeholder="Name *"
-          />
-          <input
-            className={styles.inputField}
-            type="text"
-            placeholder="Email *"
-          />
-        </div>
-        <div>
-          <input
-            className={styles.inputField}
-            type="text"
-            placeholder="Subject *"
-          />
+        <div className={styles.inputFieldContainer}>
+          <div className={styles.nameAadEmailContainer}>
+            <input
+              className={styles.inputField}
+              type="text"
+              placeholder="Name *"
+            />
+            <input
+              className={styles.inputField}
+              type="text"
+              placeholder="Email *"
+            />
+          </div>
+          <div>
+            <input
+              className={styles.inputField}
+              type="text"
+              placeholder="Subject *"
+            />
 
-          <textarea
-            className={styles.textareaField}
-            name="textarea"
-            rows="4"
-            cols="50"
-            placeholder="message"
-          />
+            <textarea
+              className={styles.textareaField}
+              name="textarea"
+              rows="4"
+              cols="50"
+              placeholder="message"
+            />
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+
+      <section>
+        <GoogleMap />
+      </section>
+    </>
   );
 };
 
---
+++ src/components/googleMap/index.js
@@ -0,0 +1,11 @@
+import styles from "./googleMap.modue.css";
+
+const GoogleMap = () => {
+  return (
+    <div>
+      <h3>ADD GOOGLE MAP</h3>
+    </div>
+  );
+};
+
+export default GoogleMap;
